8184695726b8c82f1b00a652eab7a042917aed93
Davide Benini <info@davidebenini.it>
2014-10-03 10:29:04 +0200

    Initial Commit

---
+++ UDPTesterTests/UDPTesterTests.m
@@ -0,0 +1,40 @@
+//
+//  UDPTesterTests.m
+//  UDPTesterTests
+//
+//  Created by davide on 03/10/14.
+//  Copyright (c) 2014 NT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface UDPTesterTests : XCTestCase
+
+@end
+
+@implementation UDPTesterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
